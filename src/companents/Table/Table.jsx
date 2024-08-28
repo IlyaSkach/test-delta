7b7d82940a5c22b8796a82b997ec3519393a3420
Илья С <skachilya@gmail.com>
2024-08-28 06:03:11 +0300

    ready final 1.0

--- src/companents/Table/Table.jsx
+++ src/companents/Table/Table.jsx
@@ -2,8 +2,46 @@ import React, { useState } from "react";
 import "./Table.scss";
 import Chart from '../Chart/Chart';
 
+
+
+
+const dataFirst = [
+  { method: "Выручка, руб", today: 500521, lastWeek: 480521, thisMonth: 4805121 },
+  { method: "Наличные", today: 300000, lastWeek: 300000, thisMonth: 300000 },
+  { method: "Безналичный расчет", today: 100000, lastWeek: 100000, thisMonth: 100000 },
+  { method: "Кредитные карты", today: 100521, lastWeek: 100000, thisMonth: 100521 },
+  { method: "Средний чек, руб", today: 1300, lastWeek: 900, thisMonth: 900 },
+  { method: "Средний гость, руб", today: 1200, lastWeek: 800, thisMonth: 800 },
+  { method: "Удаления из чека (после оплаты), руб", today: 1000, lastWeek: 1100, thisMonth: 900 },
+  { method: "Удаления из чека (до оплаты), руб", today: 1300, lastWeek: 1100, thisMonth: 1200 },
+  { method: "Количество чеков", today: 34, lastWeek: 36, thisMonth: 34 },
+  { method: "Количество гостей", today: 34, lastWeek: 36, thisMonth: 32 },
+];
+
+const dataSecond = [
+  { method: "Выручка, руб", today: 600521, lastWeek: 700521, thisMonth: 5805121 },
+  { method: "Наличные", today: 400000, lastWeek: 500000, thisMonth: 400000 },
+  { method: "Безналичный расчет", today: 200000, lastWeek: 100000, thisMonth: 200000 },
+  { method: "Кредитные карты", today: 200521, lastWeek: 100000, thisMonth: 200521 },
+  { method: "Средний чек, руб", today: 2300, lastWeek: 1900, thisMonth: 1900 },
+  { method: "Средний гость, руб", today: 2200, lastWeek: 1800, thisMonth: 1800 },
+  { method: "Удаления из чека (после оплаты), руб", today: 2000, lastWeek: 2100, thisMonth: 1900 },
+  { method: "Удаления из чека (до оплаты), руб", today: 2300, lastWeek: 2100, thisMonth: 2200 },
+  { method: "Количество чеков", today: 44, lastWeek: 46, thisMonth: 44 },
+  { method: "Количество гостей", today: 44, lastWeek: 46, thisMonth: 42 },
+];
+
 const Table = () => {
+  const [data, setData] = useState(dataFirst);
   const [selectedRow, setSelectedRow] = useState(null);
+
+  const handleFirstMonthClick = () => {
+    setData(dataFirst);
+  };
+
+  const handleSecondMonthClick = () => {
+    setData(dataSecond);
+  };
 
   const handleRowClick = (index) => {
     setSelectedRow(selectedRow === index ? null : index);
@@ -20,6 +58,10 @@ const Table = () => {
 
   return (
     <div className="container">
+      <div className="button-container">
+        <button className="month-button" onClick={handleFirstMonthClick}>Первый месяц</button>
+        <button className="month-button" onClick={handleSecondMonthClick}>Второй месяц</button>
+      </div>
       <table id="data-table">
         <thead>
           <tr>
@@ -67,18 +109,5 @@ const Table = () => {
     </div>
   );
 };
-
-const data = [
-  { method: "Выручка, руб", today: 500521, lastWeek: 480521, thisMonth: 4805121 },
-  { method: "Наличные", today: 300000, lastWeek: 300000, thisMonth: 300000 },
-  { method: "Безналичный расчет", today: 100000, lastWeek: 100000, thisMonth: 100000 },
-  { method: "Кредитные карты", today: 100521, lastWeek: 100000, thisMonth: 100521 },
-  { method: "Средний чек, руб", today: 1300, lastWeek: 900, thisMonth: 900 },
-  { method: "Средний гость, руб", today: 1200, lastWeek: 800, thisMonth: 800 },
-  { method: "Удаления из чека (после оплаты), руб", today: 1000, lastWeek: 1100, thisMonth: 900 },
-  { method: "Удаления из чека (до оплаты), руб", today: 1300, lastWeek: 1100, thisMonth: 1200 },
-  { method: "Количество чеков", today: 34, lastWeek: 36, thisMonth: 34 },
-  { method: "Количество гостей", today: 34, lastWeek: 36, thisMonth: 32 },
-];
 
 export default Table;
